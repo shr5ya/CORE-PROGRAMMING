72285782e2be1f74c4c2afd3bb6cf9b6d11b6d29
shr5ya <s8reya@gmail.com>
2025-07-26 21:46:34 +0530

    codes

---
+++ Level 3/ArmstrongCheck.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ArmstrongCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        int original = num, sum = 0;
+
+        while (num > 0) {
+            int digit = num % 10;
+            sum += digit * digit * digit;
+            num /= 10;
+        }
+
+        System.out.println(original + " is an Armstrong number? " + (sum == original));
+        sc.close();
+    }
+}
